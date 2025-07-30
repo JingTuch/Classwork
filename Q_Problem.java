e54d251640b9f2b92a0683d799b13e07497139c4
KinjiChuwa <164115642+JingTuch@users.noreply.github.com>
2025-07-30 14:18:07 +0800

    Add files via upload

--- Q_Problem.java
+++ Q_Problem.java
@@ -1,7 +1,13 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 class Board{
    private ArrayList<ArrayList<String>> board = new ArrayList<>();
+   Queen queen = new Queen();
+   public void showQueenPosition() {
+    Coordinate pos = queen.coordinates.get(0);
+    System.out.println("Queen is at: " + pos.x + ", " + pos.y);
+}
     public void initBoard(){
         board.clear();
         for (int i = 0; i < 8; i++) {
@@ -11,8 +17,11 @@ class Board{
             }
             board.add(currRow);
         }
-    }
-
+    } 
+   public void setPiece(int x, int y, String Piece){
+    board.get(x).set(y,Piece);
+    queen.saveCoordinate(x,y);
+   }
    public void getBoard(){
       try {
         for (int i = 0; i < 8; i++) {
@@ -25,34 +34,63 @@ class Board{
         System.out.println("Error: Tried to access an index out of bounds.");
         }
     }
-
-    public testQueen(ArrayList board){
-        int x,y;
-        int x = (int)(Math.random() * 9)
+    public void queenChecker(){
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                if(board.get(i).get(j) == "Q "){
+                    pathing(i,j);
+                }
+            }
+        }
+    }
+    public void pathing(int currx, int curry){
+         for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+        if( ((Math.abs(i - currx) == Math.abs(j - curry)) || Math.abs(i) == currx || Math.abs(j) == curry) && !(i == currx && j == curry) ){
+            board.get(i).set(j, "1 ");
+                }
+            }
+         }
+    }
+      public void placeQueen(){;
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                if(board.get(i).get(j) =="0 "){
+                    setPiece(i,j,queen.getQueen());
+                    queenChecker();
+                    
+                }
+            }
+        }
     }
 } 
+class Coordinate{
+    int x,y;
 
-class Queen{
-   private String Queen = "Q ";
-    public ArrayList coordinates = new ArrayList<>();
-    public String x,y; 
-   public void Queen (String q){
-        this.Queen = Queen;
+    public Coordinate(int x, int y){
+        this.x = x;
+        this.y = y;
     }
+}
+class Queen{
+   private String queen = "Q ";
+   public ArrayList<Coordinate> coordinates = new ArrayList<>();
+   public String x,y; 
 
    public String getQueen (){
-        return Queen;
+        return queen;
     }
-
-    public String QueenMoves(ArrayList board){
-        //get queen coordinates and highlight queen moves on board
+    public void saveCoordinate(int x, int y){
+        coordinates.add(new Coordinate(x, y));
     }
 }
 
 public class Q_Problem {
     public static void main(String[] args){
+        Queen queen = new Queen();
         Board board = new Board();
         board.initBoard();
+        board.placeQueen();
         board.getBoard();
-    }
+    }  
 }
